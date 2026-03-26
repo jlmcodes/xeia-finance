f83c648cd2d3e9419be84f4dd48ce5588cb75751
Xeia <vpacads.nudjpia2526@gmail.com>
2026-03-26 23:57:29 +0800

    huhu please get fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
-import './index.css'; // Make sure the path matches your CSS file
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-// ... rest of the file
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
